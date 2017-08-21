1b2ac3eb6764050e8e74eeabd865274f6f457f51
unknown <manvitha.eathalapaka@gmail.com>
2017-08-21 19:20:28 +1200

    Hello Program ---08/21/2017

---
+++ albums/index.android.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text , AppRegistry } from 'react-native';
+
+const App = () =>{
+return(
+  <Text>Hello</Text>
+);
+};
+AppRegistry.registerComponent('albums',() => App);
